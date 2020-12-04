97a3e82b4491b602aab7dea907f3e3d8b8553294
miguel angel <miguel.angel.cuarez@gmail.com>
2020-12-04 17:24:34 +0100

    rescatando cambios

--- controller/SessionManager.py
+++ controller/SessionManager.py
@@ -1,14 +1,17 @@
-import google.oauth2.credentials
-import google_auth_oauthlib.flow
-
+from oauth2client import client
+CLIENT_ID = '435471540706-icu11lq037akvpb6p2eep3s49f6v5icc.apps.googleusercontent.com'
+CLIENT_SECRET = 'lzIL-ekDrqJoMg5YertucG_5'
 
 class SessionManager:
     def __init__(self):
         pass
 
-    
+    def is_authorized(self, args):
+        pass
+
+    def get_credentials(self, args):
+        credentials = client.credentials_from_code(CLIENT_ID, CLIENT_SECRET, scope='', code=args['auth_code'])
+        print(credentials)
 
     
-
-
     
